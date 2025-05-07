7d1ee327048be6411b32931cf6e861136d980c62
Prashanth Ramachandran <ramapra@gmail.com>
2025-05-07 15:42:00 -0500

    Built FetchService and getPokemon function; Pokemon fetching from online successful

--- Dex/ContentView.swift
+++ Dex/ContentView.swift
@@ -15,6 +15,8 @@ struct ContentView: View {
         sortDescriptors: [NSSortDescriptor(keyPath: \Pokemon.id, ascending: true)],
         animation: .default)
     private var pokedex: FetchedResults<Pokemon>
+    
+    let fetcher = FetchService()
 
     var body: some View {
         NavigationView {
@@ -33,8 +35,41 @@ struct ContentView: View {
                 }
                 ToolbarItem() {
                     Button("Add Item", systemImage: "plus") {
-                        
+                        getPokemon()
                     }
+                }// ToolbarItem
+            } // End toolbar
+        } // Navi
+    } // End of body
+    
+    private func getPokemon() {
+        Task {
+            // for 1 < 152
+            for id in 1..<152 {
+                do {
+                    let fetchedPokemon = try await fetcher.fetchPokemon(id)
+                    let pokemon = Pokemon(context: viewContext)
+                    
+                    // Datatypes are different as one is coming via the internet while we have format which is different
+                    // this creates a object that is known to CoreData
+                    
+                    pokemon.id = fetchedPokemon.id
+                    pokemon.name = fetchedPokemon.name
+                    pokemon.types = fetchedPokemon.types
+                    pokemon.hp = fetchedPokemon.hp
+                    pokemon.attack = fetchedPokemon.attack
+                    pokemon.defense = fetchedPokemon.defense
+                    pokemon.specialAttack = fetchedPokemon.specialAttack
+                    pokemon.specialDefense = fetchedPokemon.specialDefense
+                    pokemon.speed = fetchedPokemon.speed
+                    pokemon.sprite = fetchedPokemon.sprite
+                    pokemon.shiny = fetchedPokemon.shiny
+                    
+                    // CoreData saving
+                    try viewContext.save()
+                    
+                } catch {
+                    print(error)
                 }
             }
         }
