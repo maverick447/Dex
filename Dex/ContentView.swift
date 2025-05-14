1d17eb5f1747d8c39d3f6daec2dccf44610bad43
Prashanth Ramachandran <ramapra@gmail.com>
2025-05-14 10:33:48 -0500

    Download and store images with Core Data

--- Dex/ContentView.swift
+++ Dex/ContentView.swift
@@ -62,7 +62,7 @@ struct ContentView: View {
                     Section {
                         ForEach(pokedex) { pokemon in
                             NavigationLink(value: pokemon) {
-                                AsyncImage(url: pokemon.sprite) { image in
+                                AsyncImage(url: pokemon.spriteURL) { image in
                                     image
                                         .resizable()
                                         .scaledToFit()
@@ -179,18 +179,35 @@ struct ContentView: View {
                     pokemon.specialAttack = fetchedPokemon.specialAttack
                     pokemon.specialDefense = fetchedPokemon.specialDefense
                     pokemon.speed = fetchedPokemon.speed
-                    pokemon.sprite = fetchedPokemon.sprite
-                    pokemon.shiny = fetchedPokemon.shiny
-                    
-//                    if pokemon.id  % 2 == 0  {
-//                        pokemon.favorite = true
-//                    }
+                    pokemon.spriteURL = fetchedPokemon.spriteURL
+                    pokemon.shinyURL = fetchedPokemon.shinyURL
+
+                    pokemon.sprite = try await URLSession.shared.data(from: fetchedPokemon.spriteURL).0
+                    pokemon.shiny = try await URLSession.shared.data(from: fetchedPokemon.shinyURL).0
                     // CoreData saving
                     try viewContext.save()
-                    
-                } catch {
+                // End of do
+                }  catch {
                     print(error)
                 }
+            } // For  1..152
+            
+            storeSprites()
+        } // Task
+    } // Func End
+    
+    private func storeSprites() {
+        Task {
+            do {
+                for pokemon in all {
+                    pokemon.sprite = try await URLSession.shared.data(from: pokemon.spriteURL!).0
+                    pokemon.shiny = try await URLSession.shared.data(from: pokemon.shinyURL!).0
+                    
+                    try viewContext.save()
+                    print("Sprites stored: \(pokemon.id): \(pokemon.name!.capitalized)")
+                }
+            } catch {
+                print(error)
             }
         }
     }
