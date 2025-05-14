382a001fdcc3ecf5e876dfe62cc005dc458120c5
Prashanth Ramachandran <ramapra@gmail.com>
2025-05-14 17:37:23 -0500

    Add widget extension

---
+++ DexWidget/DexWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DexWidgetBundle.swift
+//  DexWidget
+//
+//  Created by Prashanth Ramachandran on 5/14/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DexWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DexWidget()
+    }
+}
